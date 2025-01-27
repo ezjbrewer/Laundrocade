f2e74f476bd190a46eb4aff42b6cc75d77e957f0
Ezra Brewer <ezjbrewer@gmail.com>
2025-01-27 15:37:49 -0600

    Initial Commit

---
+++ client/src/components/ApplicationViews.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+import { AuthorizedRoute } from "./auth/AuthorizedRoute.jsx";
+import Login from "./auth/Login.jsx";
+import Register from "./auth/Register.jsx";
+import { Home } from "./Home.jsx";
+
+export default function ApplicationViews({ loggedInUser, setLoggedInUser }) {
+  return (
+    <Routes>
+      <Route path="/">
+        <Route
+          index
+          element={
+            <AuthorizedRoute loggedInUser={loggedInUser}>
+              <Home />
+            </AuthorizedRoute>
+          }
+        />
+        <Route
+          path="login"
+          element={<Login setLoggedInUser={setLoggedInUser} />}
+        />
+        <Route
+          path="register"
+          element={<Register setLoggedInUser={setLoggedInUser} />}
+        />
+      </Route>
+      <Route path="*" element={<p>Whoops, nothing here...</p>} />
+    </Routes>
+  );
+}
